d743b54787089a44d535de9839ca7b6328912dc7
Nagendran2005 <nagendranpackirisamy@mhcognition.in>
2026-07-15 12:20:47 +0530

    final build take (Before_Correction)

--- Assets/ProjectStructure/UnusedScripts/ProjectStructureOrganizer.cs
+++ Assets/ProjectStructure/UnusedScripts/ProjectStructureOrganizer.cs
@@ -1,128 +1,128 @@
-using UnityEngine;
-using UnityEditor;
-using System.Collections.Generic;
-using System.IO;
+//using UnityEngine;
+//using UnityEditor;
+//using System.Collections.Generic;
+//using System.IO;
 
-public class ProjectStructureOrganizer
-{
-    static string root = "Assets/ProjectStructure";
+//public class ProjectStructureOrganizer
+//{
+//    static string root = "Assets/ProjectStructure";
 
-    [MenuItem("Tools/Organize Project Scripts")]
-    static void Organize()
-    {
-        CreateFolders();
+//    [MenuItem("Tools/Organize Project Scripts")]
+//    static void Organize()
+//    {
+//        CreateFolders();
 
-        HashSet<string> usedScripts = new HashSet<string>();
-        HashSet<string> usedScriptableObjects = new HashSet<string>();
+//        HashSet<string> usedScripts = new HashSet<string>();
+//        HashSet<string> usedScriptableObjects = new HashSet<string>();
 
-        // Find scripts used in scenes
-        GameObject[] objects = GameObject.FindObjectsOfType<GameObject>(true);
+//        // Find scripts used in scenes
+//        GameObject[] objects = GameObject.FindObjectsOfType<GameObject>(true);
 
-        foreach (GameObject obj in objects)
-        {
-            MonoBehaviour[] behaviours = obj.GetComponents<MonoBehaviour>();
+//        foreach (GameObject obj in objects)
+//        {
+//            MonoBehaviour[] behaviours = obj.GetComponents<MonoBehaviour>();
 
-            foreach (var b in behaviours)
-            {
-                if (b == null) continue;
+//            foreach (var b in behaviours)
+//            {
+//                if (b == null) continue;
 
-                MonoScript script = MonoScript.FromMonoBehaviour(b);
-                string path = AssetDatabase.GetAssetPath(script);
+//                MonoScript script = MonoScript.FromMonoBehaviour(b);
+//                string path = AssetDatabase.GetAssetPath(script);
 
-                if (!string.IsNullOrEmpty(path))
-                    usedScripts.Add(path);
-            }
+//                if (!string.IsNullOrEmpty(path))
+//                    usedScripts.Add(path);
+//            }
 
-            // Check ScriptableObject references
-            SerializedObject so = new SerializedObject(obj);
-            SerializedProperty prop = so.GetIterator();
+//            // Check ScriptableObject references
+//            SerializedObject so = new SerializedObject(obj);
+//            SerializedProperty prop = so.GetIterator();
 
-            while (prop.NextVisible(true))
-            {
-                if (prop.propertyType == SerializedPropertyType.ObjectReference)
-                {
-                    Object reference = prop.objectReferenceValue;
+//            while (prop.NextVisible(true))
+//            {
+//                if (prop.propertyType == SerializedPropertyType.ObjectReference)
+//                {
+//                    Object reference = prop.objectReferenceValue;
 
-                    if (reference is ScriptableObject)
-                    {
-                        string path = AssetDatabase.GetAssetPath(reference);
-                        usedScriptableObjects.Add(path);
-                    }
-                }
-            }
-        }
+//                    if (reference is ScriptableObject)
+//                    {
+//                        string path = AssetDatabase.GetAssetPath(reference);
+//                        usedScriptableObjects.Add(path);
+//                    }
+//                }
+//            }
+//        }
 
-        // All scripts
-        string[] allScripts = AssetDatabase.FindAssets("t:MonoScript");
+//        // All scripts
+//        string[] allScripts = AssetDatabase.FindAssets("t:MonoScript");
 
-        foreach (string guid in allScripts)
-        {
-            string path = AssetDatabase.GUIDToAssetPath(guid);
+//        foreach (string guid in allScripts)
+//        {
+//            string path = AssetDatabase.GUIDToAssetPath(guid);
 
-            if (path.Contains("/Editor/"))
-            {
-                MoveAsset(path, root + "/EditorScripts");
-            }
-            else if (usedScripts.Contains(path))
-            {
-                MoveAsset(path, root + "/SceneUsedScripts");
-            }
-            else
-            {
-                MoveAsset(path, root + "/UnusedScripts");
-            }
-        }
+//            if (path.Contains("/Editor/"))
+//            {
+//                MoveAsset(path, root + "/EditorScripts");
+//            }
+//            else if (usedScripts.Contains(path))
+//            {
+//                MoveAsset(path, root + "/SceneUsedScripts");
+//            }
+//            else
+//            {
+//                MoveAsset(path, root + "/UnusedScripts");
+//            }
+//        }
 
-        // ScriptableObjects
-        string[] scriptables = AssetDatabase.FindAssets("t:ScriptableObject");
+//        // ScriptableObjects
+//        string[] scriptables = AssetDatabase.FindAssets("t:ScriptableObject");
 
-        foreach (string guid in scriptables)
-        {
-            string path = AssetDatabase.GUIDToAssetPath(guid);
+//        foreach (string guid in scriptables)
+//        {
+//            string path = AssetDatabase.GUIDToAssetPath(guid);
 
-            if (usedScriptableObjects.Contains(path))
-            {
-                MoveAsset(path, root + "/SceneScriptableObjects");
-            }
-        }
+//            if (usedScriptableObjects.Contains(path))
+//            {
+//                MoveAsset(path, root + "/SceneScriptableObjects");
+//            }
+//        }
 
-        // Custom Shaders
-        string[] shaders = AssetDatabase.FindAssets("t:Shader");
+//        // Custom Shaders
+//        string[] shaders = AssetDatabase.FindAssets("t:Shader");
 
-        foreach (string guid in shaders)
-        {
-            string path = AssetDatabase.GUIDToAssetPath(guid);
-            MoveAsset(path, root + "/CustomShaders");
-        }
+//        foreach (string guid in shaders)
+//        {
+//            string path = AssetDatabase.GUIDToAssetPath(guid);
+//            MoveAsset(path, root + "/CustomShaders");
+//        }
 
-        AssetDatabase.Refresh();
-        Debug.Log("Project Organized Successfully");
-    }
+//        AssetDatabase.Refresh();
+//        Debug.Log("Project Organized Successfully");
+//    }
 
-    static void CreateFolders()
-    {
-        if (!AssetDatabase.IsValidFolder(root))
-            AssetDatabase.CreateFolder("Assets", "ProjectStructure");
+//    static void CreateFolders()
+//    {
+//        if (!AssetDatabase.IsValidFolder(root))
+//            AssetDatabase.CreateFolder("Assets", "ProjectStructure");
 
-        Create(root, "EditorScripts");
-        Create(root, "SceneUsedScripts");
-        Create(root, "UnusedScripts");
-        Create(root, "SceneScriptableObjects");
-        Create(root, "CustomShaders");
-    }
+//        Create(root, "EditorScripts");
+//        Create(root, "SceneUsedScripts");
+//        Create(root, "UnusedScripts");
+//        Create(root, "SceneScriptableObjects");
+//        Create(root, "CustomShaders");
+//    }
 
-    static void Create(string parent, string name)
-    {
-        if (!AssetDatabase.IsValidFolder(parent + "/" + name))
-            AssetDatabase.CreateFolder(parent, name);
-    }
+//    static void Create(string parent, string name)
+//    {
+//        if (!AssetDatabase.IsValidFolder(parent + "/" + name))
+//            AssetDatabase.CreateFolder(parent, name);
+//    }
 
-    static void MoveAsset(string path, string folder)
-    {
-        string fileName = Path.GetFileName(path);
-        string newPath = folder + "/" + fileName;
+//    static void MoveAsset(string path, string folder)
+//    {
+//        string fileName = Path.GetFileName(path);
+//        string newPath = folder + "/" + fileName;
 
-        if (path != newPath)
-            AssetDatabase.MoveAsset(path, newPath);
-    }
-}
+//        if (path != newPath)
+//            AssetDatabase.MoveAsset(path, newPath);
+//    }
+//}
